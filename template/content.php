8c9c3d1cb68bbcde437bb4394f08ee92f31ce3c7
hoanglyminh <hoanglyminh@gmail.com>
2025-04-26 10:04:39 +0700

    Fix style.css

--- template/content.php
+++ template/content.php
@@ -3,7 +3,7 @@
 <div class="col-lg-4 col-md-6 about-in blog-grid-info text-left mt-5">
     <div class="card img blog-info">
         <div class="card-body img">
-            <figure class="img-thumbnails">
+            <div class="img-thumbnails">
                 <a href="<?php the_permalink() ?>" class="d-block">
                     <?php if ( has_post_thumbnail() ) { ?>                
                         <img src="<?php echo get_image_url_id(get_the_ID());?>" class="img-fluid radius-image">                                
@@ -14,7 +14,7 @@
                                 class="img-fluid radius-image">
                     <?php } ?>
                 </a>
-            </figure>
+            </div>
             <div class="blog-des">
                 <ul class="admin-post mb-3">
                     <li><span class="fas fa-user"></span> <?php the_author() ?></li>
