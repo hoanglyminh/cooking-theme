b02b97c27c4814beed5e563f192379cea1295bcc
hoanglyminh <hoanglyminh@gmail.com>
2025-08-13 10:12:00 +0700

    Fix menu entry

--- functions.php
+++ functions.php
@@ -1,14 +1,10 @@
 <?php
-    define('THEME_URL', get_template_directory_uri() );
-    define('THEME_DIR' , get_stylesheet_directory() );
-    define('CORE' , THEME_DIR . '/core_lmh' );
+    if (! defined("THEME_URL") ) { define( "THEME_URL" , get_template_directory_uri() ); }
+    if (! defined("THEME_DIR") ) { define( "THEME_DIR" , get_template_directory() ); }
+    if (! defined("CORE") ) { define( "CORE" , THEME_DIR . "/core_lmh" ); }
+    if (! defined("CORE_URL") ) { define( "CORE_URL" , THEME_URL . "/core_lmh" ); }
     
     if( file_exists( CORE . '/init.php' ) ) { 
         require_once( CORE . '/init.php' );
     }
-
-    if( class_exists("Lmh_Themes") ){
-        $lmh_themes = new Lmh_Themes();
-    }
-
 ?>
